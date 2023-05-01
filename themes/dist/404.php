21383522925dc1ef6b7a4d5f55ee71d582ac743d
almir Hadziomerovic <almir_hadziomerovic@hotmail.com>
2023-05-01 21:20:31 +0200

    404 Bild erstellen

--- themes/dist/404.php
+++ themes/dist/404.php
@@ -1,1 +1,39 @@
 <?php get_header();?>
+
+
+<main id="error">
+    <h1>
+        <?php _e('Ups, da ist was schief gegangen', 'wifi'); ?>
+    </h1>
+
+    <p>
+    <?php _e('Leider gibt es die Seite, die du gesucht hast, auf unserer Webseite nicht.
+        Doch vielleicht haben wir trotzdem etwas für dich.
+        Lass und gemeinsam nachsehen!');
+    ?>
+    </p>
+
+    <?php 
+    if (function_exists('the_custom_logo')){
+        the_custom_logo();
+    }
+    ?>
+    </div>
+    <nav class="error-menu">
+
+        <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'error',
+                    'container' => 'false',
+                    'menu_class' => 'nav-menu',
+                    'menu_id' => 'nav-error',
+                    'fallback_cb' => false,
+                    'depth' => 1
+                )
+                );
+        ?>
+    </nav>
+</main>
+
+<?php get_footer();?>
