5bb2e4fe94b01c58d5aa9a5273ceb16b635a4797
almir Hadziomerovic <almir_hadziomerovic@hotmail.com>
2023-05-14 19:49:06 +0200

    CPT Initalisieren

--- themes/dist/functions.php
+++ themes/dist/functions.php
@@ -14,10 +14,7 @@ add_action('after_setup_theme',function(){
         'script')
     );
 
-    //zusätzliche Bildgröße in WP Hinzufügen
-    add_image_size('projekt', 730, 487, array('center', center));
-
-
+    add_image_size('projekt', 730, 487, array('center', 'center'));
 
     //Logo Größe 
     add_theme_Support('custom-logo', array(
@@ -52,6 +49,7 @@ add_filter ('upload_mimes', function($mimes = array()){
     return $mimes;
 });
 
+
 //CSS Datei im Head einfügen
 add_action('wp_enqueue_scripts', function(){
     wp_enqueue_style( 'webdev-css', get_template_directory_uri(  ) . '/style.css');
@@ -64,13 +62,65 @@ add_action('wp_enqueue_scripts', function(){
 });
 
 
-//Register Custom Post Type
 
 
 
+// Register Custom Post Type in WP
+function post_type_project() {
 
+	$labels = array(
+		'name'                  => _x( 'Projekte', 'Post Type General Name', 'wifi' ),
+		'singular_name'         => _x( 'Projekt', 'Post Type Singular Name', 'wifi' ),
+		'menu_name'             => __( 'Projekte', 'wifi' ),
+		'name_admin_bar'        => __( 'Projekte', 'wifi' ),
+		'archives'              => __( 'Projekt archiv', 'wifi' ),
+		'attributes'            => __( 'Item Attributes', 'wifi' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'wifi' ),
+		'all_items'             => __( 'All Items', 'wifi' ),
+		'add_new_item'          => __( 'Neues Projekt hinzufügen', 'wifi' ),
+		'add_new'               => __( 'Neues Projekt erstellen', 'wifi' ),
+		'new_item'              => __( 'Neues Projekt', 'wifi' ),
+		'edit_item'             => __( 'Projekt bearbeiten', 'wifi' ),
+		'update_item'           => __( 'Update Item', 'wifi' ),
+		'view_item'             => __( 'View Item', 'wifi' ),
+		'view_items'            => __( 'View Items', 'wifi' ),
+		'search_items'          => __( 'Nach Projekten suchen', 'wifi' ),
+		'not_found'             => __( 'Kein Projekt gefunden', 'wifi' ),
+		'not_found_in_trash'    => __( 'Nichts im Papierkorb gefunden', 'wifi' ),
+		'featured_image'        => __( 'Featured Image', 'wifi' ),
+		'set_featured_image'    => __( 'Set featured image', 'wifi' ),
+		'remove_featured_image' => __( 'Remove featured image', 'wifi' ),
+		'use_featured_image'    => __( 'Use as featured image', 'wifi' ),
+		'insert_into_item'      => __( 'Insert into item', 'wifi' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'wifi' ),
+		'items_list'            => __( 'Items list', 'wifi' ),
+		'items_list_navigation' => __( 'Items list navigation', 'wifi' ),
+		'filter_items_list'     => __( 'Filter items list', 'wifi' ),
+	);
+	$args = array(
+		'label'                 => __( 'Haddes', 'wifi' ),
+		'description'           => __( 'Unsere Projekte', 'wifi' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 10,
+		'menu_icon'             => 'dashicons-format-gallery',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => false,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+        'show_in_rest'          => true //Muss auf true sein, wenn der Gutenberg Editor angezeigt werden soll
+	);
+	register_post_type( 'project', $args );
 
-
+}
+add_action( 'init', 'post_type_project', 0 );
 
 
 
@@ -106,18 +156,8 @@ function pagination($paged = '', $max_page = '')
 }
 
 
-
-
-
-
-
-
-
-
-// ACF Plugin Installieren bzw. ACF Einstellungen hinzufügen im Menü
 if(function_exists('acf_add_options_page')){
-
-
+    
  //ACF Felder aus den Wordpress ausgeben
  add_filter('acf/settings/save_json', function($path){
     $path = get_template_directory() . '/acf-fields';
@@ -132,7 +172,7 @@ add_filter('acf/settings/load_json', function($paths){
     return $paths;
 });
 
-
+// ACF Plugin Installieren bzw. ACF Einstellungen hinzufügen im Menü
 
     acf_add_options_page(array(
         'page_title' => 'Theme Einstellungen',
@@ -145,8 +185,6 @@ add_filter('acf/settings/load_json', function($paths){
         'capibility' => 'edit_posts'
 
     ));
-
-
 
     add_filter('block_categories_all', function($categories){
         return array_merge(
@@ -163,34 +201,29 @@ add_filter('acf/settings/load_json', function($paths){
 
 
 
-    //ACF Blöcke Initialisieren
-    add_action('acf/init', function(){
-
-        if(function_exists('acf_register_block_type')){
-
-            acf_register_block_type(array(
-                    'name' => 'webdev-header',
-                    'title' => __('Header'),
-                    'description' => __('Das ist der Headerbereich', 'wifi'),
-                    'supports' => array('anchor' => false),
-                    'category' => 'wifi',
-                    'keywords' => array('header', 'wifi', 'kopfbereich'),
-                    'post_type' => array('page'),
-                    'align' => false,
-                    'mode' => false,
-                    'icon' => 'welcome-widgets-menus',
-                    'render_template' => 'template-parts/blocks/block-header.php'
-            ));
-        }
-    }); 
 
 
 
+ //ACF Blöcke Initialisieren
+ add_action('acf/init', function(){
 
+    if(function_exists('acf_register_block_type')){
 
-
-
-
+        acf_register_block_type(array(
+                'name' => 'webdev-header',
+                'title' => __('Header'),
+                'description' => __('Das ist der Headerbereich', 'wifi'),
+                'supports' => array('anchor' => false),
+                'category' => 'wifi',
+                'keywords' => array('header', 'wifi', 'kopfbereich'),
+                'post_type' => array('page'),
+                'align' => false,
+                'mode' => false,
+                'icon' => 'welcome-widgets-menus',
+                'render_template' => 'template-parts/block-header.php'
+        ));
+    }
+}); 
 
 } else{
     add_action('admin_notice', function(){
