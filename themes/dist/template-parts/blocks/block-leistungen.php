5af4b15919be81014c2899a51b3b1e935a58510d
almir Hadziomerovic <almir_hadziomerovic@hotmail.com>
2023-07-23 21:38:16 +0200

    grid Cointainer für leistungen erstellt

--- themes/dist/template-parts/blocks/block-leistungen.php
+++ themes/dist/template-parts/blocks/block-leistungen.php
@@ -1,1 +1,41 @@
 <?php
+
+$class_name = 'leistungen'
+
+?>
+
+<?php
+$leistungen = get_field('leistungen');
+
+?>
+
+
+
+<div class="<?php echo $class_name; ?>">
+        <figure class="leistungen-img">
+            <?php
+            echo wp_get_attachment_image($leistungen['leistungen_img'], 'small', false, array('class' => 'leistungen_img animate'));
+            ?>
+        </figure>
+        <span class="leistungen_description">
+            <?php echo $leistungen['leistungen_description'];?>
+        </span>
+    <figure class="leistungen-img_Kopie">
+        <?php
+            echo wp_get_attachment_image($leistungen['leistungen_img_Kopie'], 'small', false, array('class' => 'leistungen_img animate'));
+        ?>
+    </figure>
+    <span class="leistungen_description_Kopie">
+            <?php echo $leistungen['leistungen_description_Kopie'];?>
+        </span>
+    <figure class="leistungen-img_Kopie">
+        <?php
+            echo wp_get_attachment_image($leistungen['leistungen_img_Kopie'], 'small', false, array('class' => 'leistungen_img animate'));
+        ?>
+    </figure>
+    <span class="leistungen_description_Kopie">
+            <?php echo $leistungen['leistungen_description_Kopie'];?>
+        </span>
+
+</div>
+
